59f1f6299df71a35a571886988d6d0d6ad890d26
becca4011 <becca4011@naver.com>
2020-01-18 11:00:58 +0900

    practice

---
+++ Nomad-2/2.js
@@ -0,0 +1,21 @@
+/* 
+let은 변수의 값을 바꿀 수 있지만, 
+const는 바꾸지 못함(처음 초기화 한 값으로만 사용 가능)
+되도록 const를 쓰는 것이 좋음
+*/
+
+//String
+const str = "Hello";
+console.log(str);
+
+//Boolean
+const bon = true;
+console.log(bon);
+
+//Number
+const num = 100;
+console.log(num);
+
+//Float
+const flt = 55.1;
+console.log(flt);
