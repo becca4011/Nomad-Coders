108a21d170730fc4753af50e8ca6c8ef6fc741c1
becca4011 <becca4011@naver.com>
2020-01-19 15:05:21 +0900

    practice

---
+++ Nomad-3/3.js
@@ -0,0 +1,15 @@
+//Array
+const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+console.log(daysOfWeek);
+
+//Object
+const Info = 
+{
+    name: "none",
+    age: "21",
+    gender: "Female",
+    favFood: [{name: "Kimchi", fatty: false}, {name: "pizza", fatty: true}]
+}
+
+console.log(Info);
+console.log(Info.favFood[1].name);
---
+++ Nomad-4/4.js
@@ -0,0 +1,6 @@
+function sayHello(name, age) //function sayHello(argument, argument), argument : 인자
+{
+    console.log("Hello!", name, ", you have", age, "years of age.");
+}
+
+sayHello("name", 21);
