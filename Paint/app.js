952fe846c396335602623b71cc3a9cd021179f7f
becca4011 <becca4011@naver.com>
2020-02-19 15:13:21 +0900

    practice

--- Paint/app.js
+++ Paint/app.js
@@ -3,14 +3,24 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const btnmode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
+
+const DEFAULT_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
 
 /*캔버스의 사이즈*/
-canvas.width = 700;
-canvas.height = 700;
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+/*캔버스의 배경색을 하얀색으로 설정(배경색을 바꾸지 않고 저장을 하면 배경이 투명색으로 저장되기 때문)*/
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 
 /*선의 색과 굵기(default)*/
-ctx.strokeStyle = "#2c2c2c";
+ctx.strokeStyle = DEFAULT_COLOR;
 ctx.lineWidth = 2.5;
+
+ctx.fillStyle = DEFAULT_COLOR;
 
 let painting = false;
 let filling = false;
@@ -49,6 +59,7 @@ function clickColor(event)
 {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
 function changeRange(event)
@@ -71,12 +82,36 @@ function clickMode()
     }
 }
 
+function clickCanvas()
+{
+    if(filling)
+    {
+        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE); //x, y, 가로, 세로
+    }
+}
+
+function rightClick(event)
+{
+    event.preventDefault(); //우클릭을 했을 때 메뉴가 나오지 않게 함
+}
+
+function clickSaveBtn()
+{
+    const image = canvas.toDataURL();
+    const link = document.createElement("a"); //a : anchor
+    link.href = image; //URL
+    link.download = "PaintJS";
+    link.click();
+}
+
 if(canvas)
 {
     canvas.addEventListener("mousemove", onMouseMove); //마우스를 움직일 때
     canvas.addEventListener("mousedown", startPainting); //마우스를 눌렀을 때
     canvas.addEventListener("mouseup", stopPainting); //마우스를 누르지 않을 때
     canvas.addEventListener("mouseleave", stopPainting); //마우스가 캔버스를 벗어닜을 때
+    canvas.addEventListener("click", clickCanvas); //캔버스를 클릭했을 때
+    canvas.addEventListener("contextmenu", rightClick); //우클릭을 했을 때 나오는 메뉴(저장, 복사)
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click", clickColor));
@@ -89,4 +124,9 @@ if(range)
 if(btnmode)
 {
     btnmode.addEventListener("click", clickMode);
+}
+
+if(saveBtn)
+{
+    saveBtn.addEventListener("click", clickSaveBtn);
 }
